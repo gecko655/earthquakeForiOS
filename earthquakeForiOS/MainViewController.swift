4c544dce02ce87854a321b1873b3b25d4e2695f9
gecko655 <aqwsedrft1234@yahoo.co.jp>
2014-09-07 22:20:41 +0900

    Getting status of specified url

--- earthquakeForiOS/MainViewController.swift
+++ earthquakeForiOS/MainViewController.swift
@@ -14,6 +14,9 @@ class MainViewController: UITableViewController{
     
     var swifter: Swifter? = nil
     var statuses: [JSONValue]? = nil
+    
+    @IBOutlet var urlTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         swifter!.getStatusesHomeTimelineWithCount(20, sinceID: nil, maxID: nil, trimUser: false, contributorDetails: false, includeEntities: true, success:{
@@ -36,5 +39,53 @@ class MainViewController: UITableViewController{
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as UITableViewCell
         cell.textLabel.text = statuses![indexPath.row]["text"].string
         return cell
+    }
+    @IBAction func urlButtonClicked(sender: AnyObject, forEvent event: UIEvent) {
+        fetchStatus(self.urlTextField.text)
+    }
+    
+    func fetchStatus(url: String){
+        let pattern = "https://twitter.com/[^/]*/status.*/(\\d{1,20})";
+        let regex: NSRegularExpression = NSRegularExpression.regularExpressionWithPattern(pattern, options: nil, error: nil)!
+        let resultRange = regex.firstMatchInString(url, options: nil, range: NSMakeRange(0, countElements(url))).rangeAtIndex(1).toRange()
+        let statusId = url[resultRange!].toInt()
+
+        let failureHandler: ((NSError) -> Void) = {
+            error in
+
+            self.alertWithTitle("Error", message: error.localizedDescription)
+        }
+        if(statusId != nil){
+        
+        swifter?.getStatusesShowWithID(statusId!, count: 1, trimUser: false, includeMyRetweet: false, includeEntities: true, success:
+            {(status: Dictionary<String, JSONValue>?) in
+                print(status)
+                return
+            }
+            , failure: failureHandler)
+
+      }
+    }
+    func alertWithTitle(title: String, message: String) {
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+}
+extension String
+{
+    subscript(integerIndex: Int) -> Character
+    {
+        let index = advance(startIndex, integerIndex)
+            return self[index]
+    }
+    
+    subscript(integerRange: Range<Int>) -> String
+    {
+        let start = advance(startIndex, integerRange.startIndex)
+        let end = advance(startIndex, integerRange.endIndex)
+        let range = start..<end
+        return self[range]
     }
 }
