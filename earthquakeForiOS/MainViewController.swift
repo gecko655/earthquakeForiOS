d739a49c35076da57fee3983a2f623efdb892470
gecko655 <aqwsedrft1234@yahoo.co.jp>
2014-10-18 20:20:56 +0900

    Added required icon sizes. Fixed for mobile url.

--- earthquakeForiOS/MainViewController.swift
+++ earthquakeForiOS/MainViewController.swift
@@ -89,7 +89,7 @@ class MainViewController: UITableViewController, UITextFieldDelegate{
     }
     
     func fetchStatus(url: String){
-        let pattern = "http.?://twitter.com/[^/]*/status[^/]*/(\\d{1,20})";
+        let pattern = "http.?://[^/]*twitter.com/[^/]*/status[^/]*/(\\d{1,20})";
         let regex: NSRegularExpression = NSRegularExpression.regularExpressionWithPattern(pattern, options: nil, error: nil)!
         let resultRange = regex.firstMatchInString(url, options: nil, range: NSMakeRange(0, countElements(url)))!.rangeAtIndex(1).toRange()
         let statusId = url[resultRange!]
